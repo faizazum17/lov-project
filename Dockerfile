0babb94085808c7d568a5411d96b7c793b25cb88
Faiza Zuma <81217057+faizazum17@users.noreply.github.com>
2022-12-09 23:16:55 +0700

    final commit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # For more information, please refer to https://aka.ms/vscode-docker-python
 FROM python:3.8-slim
 
-EXPOSE 8080
+# EXPOSE 8080
 
 # Keeps Python from generating .pyc files in the container
 ENV PYTHONDONTWRITEBYTECODE=1
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from database.db import engine
-import uvicorn  
 from models import user_models, question_models
 #from apis.route_general_pages import general_pages_router
 from apis.route_users import user_router
 from apis.route_question import question_router
-from apis.route_general_pages import general_pages_router
+# from apis.route_general_pages import general_pages_router
 from apis.route_attachment import attachmentstyle_router
 
 app = FastAPI()
@@ -16,13 +15,13 @@ question_models.Base.metadata.create_all(engine)
 
 app.include_router(user_router, prefix="/users")
 app.include_router(question_router, prefix="/questions")
-app.include_router(general_pages_router, prefix="/home")
+# app.include_router(general_pages_router, prefix="/home")
 app.include_router(attachmentstyle_router, prefix="/attachment-style")
 
 
 @app.get('/')
-def index():
+async def root():
     return {"Message" : "Hello, Welcome!"}
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
